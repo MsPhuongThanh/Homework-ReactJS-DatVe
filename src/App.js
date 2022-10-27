d53e353ac6efa6d8d2f36b70eeb855b872ed4005
Phuong Thanh <lethi.phuongthanh.apu@gmail.com>
2022-10-27 21:05:14 +0700

    upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import BookingTicket from './BookingFlim/BookingTicket';
+
+
+function App() {
+  return (
+    <div className="App">
+      <BookingTicket/>
+    </div>
+  );
+}
+
+export default App;
